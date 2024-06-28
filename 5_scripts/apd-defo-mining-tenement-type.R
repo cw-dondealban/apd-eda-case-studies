46870686b04d0d01665235c49976f42831bf5ef4
cw-dondealban <don@clearwind.earth>
2024-06-28 12:59:58 +0800

    Saved changes to script

--- 5_scripts/apd-defo-mining-tenement-type.R
+++ 5_scripts/apd-defo-mining-tenement-type.R
@@ -1,20 +1,20 @@
-# Load Libraries -------------------------
+# LOAD LIBRARIES -------------------------
 library(reshape2)
 library(tidyverse)
 
-# Define Working Directories -------------
+# DEFINE WORKING DIRECTORIES -------------
 DirMAIN <- "/Users/dondealban/Library/CloudStorage/OneDrive-ClearWindPteLtd/GitHub Repositories/apd-eda-case-studies/"
 DirSRCE = paste(DirMAIN, "0_source/", sep = "")
 DirEXTR = paste(DirMAIN, "1_extract/", sep = "")
 DirTRAN = paste(DirMAIN, "2_transform/", sep = "")
-DirLOAD = paste(DirMAIN, "3_transform/", sep = "")
-DirOUTP = paste(DirMAIN, "4_load/", sep = "")
+DirLOAD = paste(DirMAIN, "3_load/", sep = "")
+DirOUTP = paste(DirMAIN, "4_output/", sep = "")
 
-# Read Source Data Files -----------------
+# READ SOURCE DATA FILES -----------------
 setwd(DirSRCE)
 csvDEFO <- read.csv(file="raw-data-defo-cadt-mining-tenement type.csv", header=TRUE, sep=",")
 
-# Transform Data File -------------------
+# TRANSFORM DATA FILE --------------------
 
 # Convert csv input file into dataframe
 dfDEFO <- csvDEFO
@@ -33,22 +33,20 @@ tranDEFO[tranDEFO=="Coal"] <- "COAL"
 tranDEFO[tranDEFO=="-"] <- "UNKNOWN"
 
 # Save dataframe as csv file in transform folder
-write.csv(tranDEFO, paste(DirTRAN, "tranDEFO.csv", sep=""), row.names=FALSE)
+write.csv(tranDEFO, paste(DirTRAN, "transform-defo-cadt-mining-tenement type.csv", sep=""), row.names=FALSE)
 
-
-
-# Generate Plots -------------------------
+# GENERATE PLOTS -------------------------
 
 # Deforestation within each mining tenement type inside each ancestral domain
-plotDEFO <- ggplot() + geom_bar(data=dfDEFO, aes(x=factor(CADT.Number), y=Defo.Full.HRP, fill="#c6c3bf"), stat="identity", position=position_dodge())
+plotDEFO <- ggplot() + geom_bar(data=tranDEFO, aes(x=factor(CADT.Number), y=Defo.Full.HRP, fill="#000000"), stat="identity", position=position_dodge())
+plotDEFO <- plotDEFO + scales_fill_manual()
+
 plotDEFO <- plotDEFO + facet_wrap(~Tenement.Type, scales="free_y")
-plotDEFO <- plotDEFO + labs(x="CADT Number", y="Total Area of Deforestation (ha; HRP 2013-2022)")
+plotDEFO <- plotDEFO + labs(x="CADT Number", y="Total Area of Deforestation (ha) during HRP 2013-2022")
 plotDEFO <- plotDEFO + guides(fill="none")
 plotDEFO <- plotDEFO + theme(axis.text.x=element_text(size=5))
 plotDEFO
 
-# Deforestation along existing roads across buffer distances
-
-
-# Save Output Plots ----------------------
-ggsave(plotDEFO, file="Defo-CADT-MiningTenement.pdf", width=30, height=15, units="cm", dpi=300)
+# Save output plots in output folder
+setwd(DirOUTP)
+ggsave(plotDEFO, file="Defo-CADT-Mining-Tenement-Type.pdf", width=30, height=15, units="cm", dpi=300)
